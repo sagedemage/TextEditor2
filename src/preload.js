7d8d199a4937b4750ef975ab3e5a90982ed5cf60
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-27 14:20:11 -0500

    removed unused handlers

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    setTitle: (title) => ipcRenderer.send('set-title', title),
-    handleCounter: (callback) => ipcRenderer.on('update-counter', callback),
     handleSetContent: (callback) => ipcRenderer.on('set-content', callback),
     handleGetContent: (callback) => ipcRenderer.on('get-content', callback)
 })
