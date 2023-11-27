5fbe6ed990884a85443b10169fdc21520e5ba91d
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-27 14:09:07 -0500

    removed the extra html and renderer code

--- src/renderer.js
+++ src/renderer.js
@@ -1,21 +1,5 @@
 /* Renderer */
 
-const setButton = document.getElementById('btn')
-const titleInput = document.getElementById('title')
-setButton.addEventListener('click', () => {
-    const title = titleInput.value
-    window.electronAPI.setTitle(title)
-})
-
-const counter = document.getElementById('counter')
-
-window.electronAPI.handleCounter((event, value) => {
-    const oldValue = Number(counter.innerText)
-    const newValue = oldValue + value
-    counter.innerText = newValue
-    event.sender.send('counter-value', newValue)
-})
-
 window.electronAPI.handleSetContent((event, value) => {
     /* Set text area content */
     document.getElementById('text_box').value = `${value}`
