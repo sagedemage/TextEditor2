bba877029bd14f3fc0363c394bdc6d8cd6637478
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-28 14:43:56 -0500

    ignore .vite directory

--- .vite/build/preload.js
+++
@@ -1,1 +0,0 @@
-"use strict";const{contextBridge:t,ipcRenderer:n}=require("electron");t.exposeInMainWorld("electronAPI",{handleSetContent:e=>n.on("set-content",e),handleGetContent:e=>n.on("get-content",e)});
